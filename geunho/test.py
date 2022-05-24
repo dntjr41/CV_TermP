106976d3ba95ad750a86e89bef445400c147d489
방근호 <72342550+banggeunho@users.noreply.github.com>
2022-05-24 16:06:49 +0900

    Add files via upload

--- geunho/test.py
+++ geunho/test.py
@@ -9,53 +9,50 @@ from model.res_unet.res_unet import ResUnet
 from model.res_unet.res_unet_plus import ResUnetPlusPlus
 from model.res_unet.unet import UNet
 
-def main():
+
+def test():
+    device = "cpu"
+    if torch.cuda.is_available():
+      device = "cuda:0"
+      print('device 0 :', torch.cuda.get_device_name(0))
     # Change to your data root directory
     root_path = "./cv_project"
-    check_point = './checkpoints/modelname.pt'
+    check_point = './checkpoints/best_model.pt'
+
     # Depend on runtime setting
-    use_cuda = False
+    use_cuda = True
 
     test_dataset = ColorHintDataset(root_path, 256, "test")
 
     dataloaders = {}
-    dataloaders['test'] = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=True)
-
+    dataloaders['test'] = torch.utils.data.DataLoader(test_dataset, shuffle=False)
     print('test dataset: ', len(test_dataset))
 
-    model = ResUnetPlusPlus(3)
 
-    # model = Unet().cuda()
+    # state_dict = torch.load(check_point)
+    model = ResUnet(3).to(device)
+
     model.load_state_dict(torch.load(check_point))
-    os.makedirs('outputs/predict', exist_ok=True)
 
-    print('test dataset: ', len(dataloaders['test']))
+    os.makedirs('outputs/test', exist_ok=True)
 
     model.eval()
     for i, data in enumerate(tqdm.tqdm(dataloaders['test'])):
         if use_cuda:
-            l = data["l"].to('cuda')
-            ab = data["ab"].to('cuda')
-            hint = data["hint"].to('cuda')
-            mask = data["mask"].to('cuda')
+            l = data["l"].to(device)
+            hint = data["hint"].to(device)
+            mask = data["mask"].to(device)
         else:
             l = data["l"]
-            ab = data["ab"]
             hint = data["hint"]
             mask = data["mask"]
 
-        filename = data["file_name"]
+        file_name = data["file_name"]
 
-        hint_image = torch.cat((l, ab, mask), dim=1)
+        hint_image = torch.cat((l, hint), dim=1)
 
         output_hint = model(hint_image)
         out_hint_np = tensor2im(output_hint)
         output_bgr = cv2.cvtColor(out_hint_np, cv2.COLOR_LAB2BGR)
 
-        fname = str(filename).replace("['", '')
-        fname = fname.replace("']", '')
-
-        cv2.imwrite('./outputs/predict/' + str(fname), output_bgr)
-
-if __name__ == '__main__':
-    main()
+        cv2.imwrite("outputs/test/"+file_name, output_bgr)
